d02a2462c99590de02b0dfce6bd7c620065bb1a6
Gloria <gjchen@ucdavis.edu>
2015-08-24 13:30:47 -0700

    Revert to commit 6d7deed

--- LetsWorkOut/Pods/Target Support Files/MZTimerLabel/MZTimerLabel-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MZTimerLabel.h"
-
-FOUNDATION_EXPORT double MZTimerLabelVersionNumber;
-FOUNDATION_EXPORT const unsigned char MZTimerLabelVersionString[];
-
--- LetsWorkOut/Pods/Target Support Files/youtube-ios-player-helper/youtube-ios-player-helper-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "YTPlayerView.h"
-
-FOUNDATION_EXPORT double youtube_ios_player_helperVersionNumber;
-FOUNDATION_EXPORT const unsigned char youtube_ios_player_helperVersionString[];
-
